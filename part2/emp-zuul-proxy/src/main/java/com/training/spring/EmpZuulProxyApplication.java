ce223fe7a3b3971f1d03d03e1b524c8f942e5a78
vivek <vivek.check91@gmail.com>
2018-06-22 17:58:01 +0530

    Add files via upload

--- part2/emp-zuul-proxy/src/main/java/com/training/spring/EmpZuulProxyApplication.java
+++ part2/emp-zuul-proxy/src/main/java/com/training/spring/EmpZuulProxyApplication.java
@@ -11,4 +11,20 @@ public class EmpZuulProxyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmpZuulProxyApplication.class, args);
 	}
+	
+	/*
+	 * Check below url
+	 * 
+	 * http://localhost:9080/api/redirect
+	 * 		--> calls redirect
+	 * 
+	 * http://localhost:9080/api/host/emp/get/100
+	 * 		--> calls host (emp-client-service project)
+	 * 
+	 * http://localhost:9080/api/service/app/emp?id=100
+	 * 		--> calls by service (spring-boot-1 project)
+	 * 
+	 * 
+	 * 
+	 */
 }
